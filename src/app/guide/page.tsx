db351bb5c85aed91f7cea3ebe13895b698a6fa7f
khantzeyar <khla0002@student.monash.edu>
2025-09-01 10:33:41 +0800

    Added work in progress notice to guide page

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+
 const GuidePage = () => {
   return (
-    <div>guide</div>
+    <div className="flex flex-col items-center justify-center min-h-[60vh]">
+      <div className="mb-6 px-6 py-3 bg-yellow-100 border-l-4 border-yellow-500 text-yellow-800 rounded shadow text-center">
+        <span className="font-semibold">🚧 Page is a work in progress</span><br/>
+        We&apos;re still building this section. Please check back soon!
+      </div>
+    </div>
   );
 };
 
